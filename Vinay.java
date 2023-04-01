d8b6fa8fc51382ce5d140ec5e86b25e1de0a7ef4
vinaysalunkhe <vinaysalunkhe1006@gmail.com>
2023-04-01 23:04:54 +0530

    add function added

--- Vinay.java
+++ Vinay.java
@@ -3,4 +3,8 @@ class Vinay
     public static void main(String args[]) {
         System.out.println("manish");
     }
+   public static int add(int a,int b,int c)
+    {
+        return a+b+c;
+    }
 }
